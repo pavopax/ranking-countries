f64a2c653cdec24e33c615871f4ade7409e541db
paul <pavopax@gmail.com>
2016-03-29 17:25:34 -0400

    updates

--- planning/scrape-usn-data.py
+++ planning/scrape-usn-data.py
@@ -4,6 +4,7 @@ from bs4 import BeautifulSoup   # html parser
 from collections import namedtuple # like a dict
 from urlparse import urljoin 
 import re
+import warnings
 
 def dl_data(url):
     print "Downloading data..."
@@ -20,7 +21,7 @@ def get_countries(raw):
     soup = BeautifulSoup(raw.text)
     divs = soup.find_all('div', attrs={'class': 'small-12 column'})
     p = divs[1].select('p')[0]      # only one exists
-    return [get_country_info(c) for c in p.select('a') if get_country_data(c) is not None]
+    return [get_country_info(c) for c in p.select('a') if get_country_info(c) is not None]
 
 
 def get_attr(item):
@@ -30,20 +31,21 @@ def get_attr(item):
 
 def fixit(text):
     t = text.strip()
+    t = t.replace("(", "")
     try:
         float(t)
         return float(t)
     except ValueError:
-        print "FOUND ERROR. Returning zero"
-        raise SystemExit()
-        #return 0
-        #return False
+        warnings.warn("Warning: Did not get a float where needed.")
+        return False
     
 def get_attributes(raw):
     soup = BeautifulSoup(raw.text)
     div = soup.select('span#docs-internal-guid-c27701e2-0e7e-7a4a-d5bd-469d49804385')
     aa = div[0].find_all('a')
+    # extract percent values of weights
     x = re.findall('<b>(\S?\d+\.\d+ )percent\S?<\/b>', str(div))
+    print x
     x = [fixit(t) for t in x]
     print x
     return [get_attr(attr) for attr in aa if get_attr(attr) is not None]
