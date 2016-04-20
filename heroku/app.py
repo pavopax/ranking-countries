238166ca46984707f340c43c4ff34ab9e23a15ec
paul <pavopax@gmail.com>
2016-04-20 12:05:07 -0400

    major heroku app updates: add database, edit data explorer

--- heroku/app.py
+++ heroku/app.py
@@ -1,6 +1,5 @@
 import os
 import wbdata
-import time
 import datetime
 import pandas as pd
 import numpy as np
@@ -10,7 +9,7 @@ import urlparse
 from flask import Flask, render_template, send_from_directory, request,redirect, flash
 from bokeh.plotting import figure
 from bokeh.embed import components
-from flask.ext.sqlalchemy import SQLAlchemy
+#from flask.ext.sqlalchemy import SQLAlchemy
 
 # my forms.py file
 from forms import IndicatorForm
@@ -21,8 +20,6 @@ app = Flask(__name__)
 app.config.update(
     DEBUG=True,
 )
-
-
 
 
 # for csrf stuff (?)
@@ -74,82 +71,55 @@ def explorer():
         return render_template('explorer.html', form = form)
 
 
-# @app.route('/explorer', methods = ['GET', 'POST'])
-# def contact():
-#     form = IndicatorForm()
-   
-#     if request.method == 'POST':
-#         if form.validate() == False:
-#             flash('All fields are required.')
-#             return render_template('explorer.html', form = form)
-#         else:
-#             return redirect('/graph')
-#     elif request.method == 'GET':
-#         return render_template('explorer.html', form = form)
-
-
-# @app.route("/explorer", methods=['GET', 'POST'])
-# def explorer():
-#     if request.method=='GET':
-#         return render_template('explorer.html')
-#     else:
-#         app.vars['ix'] = request.form['indicator']
-#         return redirect('/graph')
-
-# @app.route("/explorer", methods=['GET', 'POST'])
-# def explorer():
-#     if request.method=='GET':
-#         return render_template('explorer.html')
-#     else:
-#         app.vars['ix'] = request.form['indicator']
-#         return redirect('/graph')
-
-
 @app.route('/graph')
 def graph():
+    """get data from database, put into pandas, and display.
+    data (indicators) are from user input in explorer()
+    """
+    urlparse.uses_netloc.append("postgres")
+    url = urlparse.urlparse(os.environ["DATABASE_URL"])
 
-    ## TODO: automate this.
-    ## select USN's top 60 countries (scraping is DONE)
-    countries = ["ARG", "AUS", "AUT", "AZE", "BGR", "BOL", "BRA", "CAN", "CHL", "CHN", "COL", "CRI", "CZE", "DEU", "DNK", "DOM", "DZA", "EGY", "ESP", "FRA", "GBR", "GRC", "GTM", "HUN", "IDN", "IND", "IRL", "IRN", "ISR", "ITA", "JOR", "JPN", "KAZ", "KOR", "LKA", "LUX", "MAR", "MEX", "MYS", "NGA", "NLD", "NZL", "PAK", "PAN", "PER", "PHL", "PRT", "ROU", "RUS", "SAU", "SGP", "SWE", "THA", "TUN", "TUR", "UKR", "URY", "USA", "VNM", "ZAF"]
+    conn = psycopg2.connect(
+        database=url.path[1:],
+        user=url.username,
+        password=url.password,
+        host=url.hostname,
+        port=url.port
+    )
 
-    # set up the indicator I want
-    # (just build up the dict if you want more than one)
-    oldindicators = {'NY.GDP.PCAP.CD': 'GDP per capita',
-                  'SI.POV.GINI':'GINI',
-                  'AG.LND.TOTL.K2':'Land Area'}
+    cursor = conn.cursor()
 
+    # from user input
     ind1 = app.vars['i1']
     ind2 = app.vars['i2']
-    #landarea = 'AG.LND.TOTL.K2'
-    keys = [ind1] + [ind2] 
-    vals = [ind1] + [ind2] 
 
-    indicators = dict(zip(keys, vals))
- 
-    #TODO edit date. using some recent date for now
-    dates = datetime.datetime(2001, 1, 1, 0, 0)
+    query = """SELECT "Country", "Indicator", "zscore" from usn WHERE "Indicator" IN (%s, %s);"""
+    cursor.execute(query, (ind1, ind2) )
 
-    df0 = wbdata.get_dataframe(indicators, country=countries, data_date=dates,
-                           convert_date=False)
+    df=pd.DataFrame(cursor.fetchall(), columns=['Country', 'Indicator',
+                                                'zscore'])
+    df = df.pivot('Country', 'Indicator')
+    df = df.zscore
 
-    # standardize the 2 indicators, but transform the land area (3rd column)
-    df1 = df0.ix[:,0:2].apply(lambda x: (x - np.min(x)) /(np.max(x)-np.min(x)))
-    # df2 = df0.ix[:,2]
-    # df2 = 5*np.log(df2)/((np.log(df2.max()))-(np.log(df2.min())+5)).astype(
-    #     np.float64)
+    # get indicator labels for axes
+    query = """SELECT "Indicator", "Label" from codes WHERE "Indicator" IN (%s, %s);"""
+    cursor.execute(query, (ind1, ind2) )
+    labs = cursor.fetchall()
 
-    df = df1
-    # TODO: use HoverTools (SEE ipy nb)
-    ptitle=str(df.columns[1]) + " vs. " + str(
-        df.columns[0]) +" (standardized values)"
+    conn.commit();
+    conn.close();
+
     p = figure(width=700, height=500, title = app.vars['title'],
-               x_axis_label = df.columns[1], y_axis_label = df.columns[0])
-    p.circle(x=df[df.columns[1]], y=df[df.columns[0]],
-             size=10, color="navy", alpha=0.5)
+	       x_axis_label = labs[0][1], y_axis_label = labs[1][1])
+    p.circle(x=df[df.columns[0]], y=df[df.columns[1]], size=10,
+	     color="navy", alpha=0.5)
+
+    p.xaxis.axis_label_text_font_size = "10pt"
+    p.yaxis.axis_label_text_font_size = "10pt"
 
     script, div = components(p)
     return render_template('graph.html', script=script, div=div,
-                           i1=ind1,i2 = ind2)
+                           i1=ind1, i2=ind2)
 
 
 @app.route("/ranking")
@@ -177,42 +147,8 @@ def week1():
 
 @app.route("/testing")
 def testing():
-    # https://devcenter.heroku.com/articles/heroku-postgresql#connecting-in-python
-    urlparse.uses_netloc.append("postgres")
-    url = urlparse.urlparse(os.environ["DATABASE_URL"])
-
-    conn = psycopg2.connect(
-        database=url.path[1:],
-        user=url.username,
-        password=url.password,
-        host=url.hostname,
-        port=url.port
-    )
-
-    # http://stackoverflow.com/questions/31473457/converting-query-results-into-dataframe-in-python
-    # QUOTES ARE important: """ vs ' vs "
-    # http://www.postgresql.org/docs/current/static/sql-syntax-lexical.html#SQL-SYNTAX-IDENTIFIERS
-    #query = """SELECT "Country", "Indicator", "zscore" from usn"""
-    query = """SELECT "Country", "Indicator", "zscore" from usn WHERE "Indicator"
-    IN ('AG.LND.TOTL.K2', 'SI.POV.GINI');"""
-
-    cursor = conn.cursor()
-    cursor.execute(query)
-
-    df=pd.DataFrame(cursor.fetchall(), columns=['Country', 'Indicator', 'zscore'])
-    df = df.pivot('Country', 'Indicator')
-    df = df.zscore
-
-    conn.commit();
-    conn.close();
-
-    p = figure(width=700, height=500, title = 'my title',
-	       x_axis_label = df.columns[1], y_axis_label = df.columns[0])
-    p.circle(x=df[df.columns[1]], y=df[df.columns[0]], size=10,
-	     color="navy", alpha=0.5)
-
-    script, div = components(p)
-    return render_template('testing.html',  script=script, div=div)
+    """ this is a testing page """
+    return render_template('testing.html')
 
 # launch
 if __name__ == "__main__":
--- heroku/forms.py
+++ heroku/forms.py
@@ -10,18 +10,51 @@ from wtforms import validators, ValidationError
 #indicators = [('AG.LND.TOTL.K2':'Land Area'), ('NY.GDP.PCAP.CD', 'GDP per capita (current US$)'), ('SP.POP.TOTL', 'Population, total')]
 
 
-indicators = [('AG.LND.TOTL.K2','Land Area'), ('NY.GDP.PCAP.CD', 'GDP per capita (current US$)'),
-              ('SP.POP.TOTL', 'Population, total')]
+# TODO: get these from DB
+indicators = [('AG.LND.TOTL.K2', 'Land area (sq. km)'),
+              ('EN.POP.DNST', 'Population density (people per sq. km of land area)'),
+              ('ER.BDV.TOTL.XQ', 'GEF benefits index for biodiversity (0 = no biodiversity potential to 100 = maximum)'),
+              ('ST.INT.ARVL', 'International tourism, number of arrivals'),
+              ('ST.INT.RCPT.XP.ZS', 'International tourism, receipts (% of total exports)'),
+              ('SE.ADT.LITR.ZS', 'Literacy rate, adult total (% of people ages 15 and above)'),
+              ('IC.BUS.NDNS.ZS', 'New business density (new registrations per 1,000 people ages 15-64)'),
+              ('FB.CBK.BRCH.P5', 'Commercial bank branches (per 100,000 adults)'),
+              ('SL.TLF.TERT.ZS', 'Labor force with tertiary education (% of total)'),
+              ('SL.TLF.SECO.ZS', 'Labor force with secondary education (% of total)'),
+              ('TX.VAL.TECH.CD', 'High-technology exports (current US$)'),
+              ('TX.VAL.TECH.MF.ZS', 'High-technology exports (% of manufactured exports)'),
+              ('IC.BUS.DISC.XQ', 'Business extent of disclosure index (0=less disclosure to 10=more disclosure)'),
+              ('IS.RRS.GOOD.MT.K6', 'Railways, goods transported (million ton-km)'),
+              ('IS.AIR.PSGR', 'Air transport, passengers carried'),
+              ('IC.LGL.CRED.XQ', 'Strength of legal rights index (0=weak to 12=strong)'),
+              ('IC.REG.DURS', 'Time required to start a business (days)'),
+              ('IC.BUS.EASE.XQ', 'Ease of doing business index (1=most business-friendly regulations)'),
+              ('NV.IND.MANF.ZS', 'Manufacturing, value added (% of GDP)'),
+              ('NY.GDP.MKTP.CD', 'GDP (current US$)'),
+              ('NY.GDP.PCAP.CD', 'GDP per capita (current US$)'),
+              ('NY.GDP.PCAP.PP.CD', 'GDP per capita, PPP (current international $)'),
+              ('GC.TAX.YPKG.ZS', 'Taxes on income, profits and capital gains (% of total taxes)'),
+              ('SP.POP.GROW', 'Population growth (annual %)'),
+              ('MS.MIL.XPND.CN', 'Military expenditure (current LCU)'),
+              ('MS.MIL.XPND.GD.ZS', 'Military expenditure (% of GDP)'),
+              ('SL.UEM.TOTL.ZS', 'Unemployment, total (% of total labor force) (modeled ILO estimate)'),
+              ('FP.CPI.TOTL.ZG', 'Inflation, consumer prices (annual %)'),
+              ('SI.POV.GINI', 'GINI index (World Bank estimate)'),
+              ('SH.MED.BEDS.ZS', 'Hospital beds (per 1,000 people)')]
+
+
 
 class IndicatorForm(Form):
     #title = TextField("Custom Plot Title",[validators .Required("IMPORTANT NOTE: Please enter a title. <<< ")])
-    title = TextField("Custom Plot Title")
    
-    indicator1 = SelectField('First Indicator',
+    indicator1 = SelectField('Vertical (Y) Axis',
                            choices = indicators)
 
-    indicator2 = SelectField('Second Indicator',
+    indicator2 = SelectField('Horizontal (X) Axis',
                            choices = indicators)
 
-    submit = SubmitField("Create a fancy Bokeh plot!")
+    title = TextField("Add a Custom Plot Title", default="My Fancy Plot Title")
+
+
+    submit = SubmitField("Submit")
 
