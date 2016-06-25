21b27f2e4585df37c4f6c7dabadd3f138a7e8727
paul <pavopax@gmail.com>
2016-06-24 22:55:42 -0400

    [new] add script to generate indicator list for app

---
+++ code/generate-indicator-list.py
@@ -0,0 +1,30 @@
+"""Print list of indicators for web app to stdout. Paste these into
+/heroku/forms.py , instead of loading from database each time
+
+"""
+
+
+import psycopg2
+import pandas as pd
+
+def make_df(query):
+    query = query
+    cursor.execute(query)
+
+    columns = [desc[0] for desc in cursor.description]
+    return pd.DataFrame(cursor.fetchall(), columns=columns)
+
+
+conn = psycopg2.connect("dbname='indicators'")
+cursor = conn.cursor()
+
+# this not strictly necessary if all indicators are in `metadata`
+query = """SELECT distinct indicator from indicators;"""
+indicator_df = make_df(query)
+
+query = """SELECT indicator, label_short from metadata;"""
+label_df = make_df(query)
+
+df = indicator_df.merge(label_df)
+
+print [tuple(x) for x in df.to_records(index=False)]
